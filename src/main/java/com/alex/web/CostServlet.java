cdc6b2863686a3d8cf34e61acf1978e2c481150d
Alex <alexandr.jelia@gmail.com>
2023-01-06 13:58:12 +0300

    servlet spring context

--- src/main/java/com/alex/web/CostServlet.java
+++ src/main/java/com/alex/web/CostServlet.java
@@ -1,11 +1,14 @@
 package com.alex.web;
 
 import com.alex.model.Cost;
-import com.alex.repository.CostRepository;
 import com.alex.repository.inmemory.InMemoryCostRepository;
 import com.alex.util.CostsUtil;
-import org.slf4j.Logger;
+import com.alex.web.cost.CostRestController;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.StringUtils;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,29 +19,37 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Objects;
 
-import static org.slf4j.LoggerFactory.getLogger;
-
 public class CostServlet extends HttpServlet {
-    private static final Logger log = getLogger(CostServlet.class);
-    private CostRepository repository;
+    private ConfigurableApplicationContext springContext;
+    private CostRestController costController;
 
     @Override
-    public void init() {
-        repository = new InMemoryCostRepository();
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        springContext = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        costController = springContext.getBean(CostRestController.class);
+    }
+
+    @Override
+    public void destroy() {
+        springContext.close();
+        super.destroy();
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws  IOException {
         request.setCharacterEncoding("UTF-8");
-        String id = request.getParameter("id");
 
-        Cost cost = new Cost(id.isEmpty() ? null : Integer.valueOf(id),
+        Cost cost = new Cost(
                 LocalDateTime.parse(request.getParameter("dateTime")),
                 request.getParameter("description"),
                 Integer.parseInt(request.getParameter("cost")));
 
-        log.info(cost.isNew() ? "Create {}" : "Update {}", cost);
-        repository.save(cost, SecurityUtil.authUserId());
+        if(StringUtils.isEmpty(request.getParameter("id"))){
+            costController.create(cost);
+        }else{
+            costController.update(cost,getId(request));
+        }
         response.sendRedirect("costs");
     }
 
@@ -49,22 +60,20 @@ public class CostServlet extends HttpServlet {
         switch (action == null ? "all" : action) {
             case "delete":
                 int id = getId(request);
-                log.info("Delete id={}", id);
-                repository.delete(id, SecurityUtil.authUserId());
+                costController.delete(id);
                 response.sendRedirect("costs");
                 break;
             case "create":
             case "update":
                 final Cost cost = "create".equals(action) ?
                         new Cost(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) :
-                        repository.get(getId(request), SecurityUtil.authUserId());
+                        costController.get(getId(request));
                 request.setAttribute("cost", cost);
                 request.getRequestDispatcher("/costForm.jsp").forward(request, response);
                 break;
             case "all":
             default:
-                log.info("getAll");
-                request.setAttribute("costsList", CostsUtil.getTransferObjects(repository.getAll(SecurityUtil.authUserId()), CostsUtil.DEFAULT_COSTS_PER_DAY));
+                request.setAttribute("costsList", costController.getAll());
                 request.getRequestDispatcher("/costs.jsp").forward(request, response);
                 break;
         }
