62acd8342129d43d22d472898394dc9dcd1a5ca4
Alex <alexandr.jelia@gmail.com>
2023-02-09 13:11:45 +0300

    deny users page for role user

--- src/main/java/com/alex/web/RootController.java
+++ src/main/java/com/alex/web/RootController.java
@@ -2,6 +2,7 @@ package com.alex.web;
 
 import com.alex.service.CostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,8 @@ public class RootController {
         return "redirect:costs";
     }
 
+//    @Secured("ROLE_ADMIN")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/users")
     public String getUsers() {
         return "users";
